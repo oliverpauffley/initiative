022be31d33a20bd0f311cbdbe85a002621fb8a71
oliverpauffley <mrpauffley@gmail.com>
2026-02-27 21:28:56 +0000

    fix nix

--- hydra-jobs.nix
+++ hydra-jobs.nix
@@ -3,11 +3,13 @@
 let
   pkgs = import nixpkgs { };
   hlib = pkgs.haskell.lib;
-  initiative = pkgs.haskellPackages.callPackage ./initiative.nix {
-    postInstall = ''
-      cp -r $src/sql $out/bin/sql
-    '';
-  };
+  initiative =
+    hlib.overrideCabal (pkgs.haskellPackages.callPackage ./initiative.nix)
+    (old: {
+      postInstall = ''
+        cp -r $src/sql $out/bin/sql
+      '';
+    });
   initiative-integration =
     hlib.overrideCabal initiative (old: { checkPhase = ""; });
 
