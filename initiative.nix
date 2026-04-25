dee0628e0b12e3f03e134c2b77e5dccb84d863e3
oliverpauffley <mrpauffley@gmail.com>
2026-04-25 15:22:02 +0100

    update nix deps

--- initiative.nix
+++ initiative.nix
@@ -1,9 +1,9 @@
 { mkDerivation, aeson, array, base, case-insensitive, co-log
-, hedgehog, hoauth2, hspec, hspec-core, hspec-discover, http-client
-, http-conduit, http-types, lens, lib, mtl, postgresql-simple
-, relude, resource-pool, servant, servant-client, servant-server
-, text, time, tomland, uri-bytestring, uuid, wai, wai-extra, warp
-, zlib
+, hedgehog, hoauth2, hspec, hspec-core, hspec-discover
+, hspec-hedgehog, http-client, http-conduit, http-types, lens, lib
+, mtl, nonempty-containers, postgresql-simple, relude
+, resource-pool, servant, servant-client, servant-server, text
+, time, tomland, uri-bytestring, uuid, wai, wai-extra, warp, zlib
 }:
 mkDerivation {
   pname = "initiative";
@@ -14,15 +14,18 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson array base case-insensitive co-log hedgehog hoauth2 hspec
     hspec-core http-client http-conduit http-types lens mtl
-    postgresql-simple relude resource-pool servant servant-client
-    servant-server text time tomland uri-bytestring uuid wai wai-extra
-    warp
+    nonempty-containers postgresql-simple relude resource-pool servant
+    servant-client servant-server text time tomland uri-bytestring uuid
+    wai wai-extra warp
   ];
   executableHaskellDepends = [
     base co-log hspec hspec-core mtl relude resource-pool time uuid
   ];
   executablePkgconfigDepends = [ zlib ];
-  testHaskellDepends = [ base hedgehog hspec hspec-core mtl relude ];
+  testHaskellDepends = [
+    base hedgehog hspec hspec-core hspec-hedgehog mtl
+    nonempty-containers relude
+  ];
   testToolDepends = [ hspec-discover ];
   doHaddock = false;
   description = "Organizing TTRPG and other similar groups to find timeslots for games";
